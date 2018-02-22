d851b3340eab6eaeff3122be89a18c55d740ee33
enitram <martin.engler@gmail.com>
2018-02-22 14:18:57 +0100

    fixed truncated nauty output

--- charge/nauty.py
+++ charge/nauty.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 import subprocess
+from io import BytesIO
 from itertools import groupby
 from typing import Any, Dict, Tuple
 
@@ -58,13 +59,15 @@ class Nauty:
         self.__process.stdin.write(cmd.encode())
         self.__process.stdin.flush()
 
-        out = self.__process.stdout.read(2**16).decode()
+        out = self.__process.stdout.read(1000)
+        while not b'END' in out:
+            out += self.__process.stdout.read(1000)
 
-        return self.__nauty_output(out.strip(), graph, idx, color_key, core)
+        return self.__nauty_output(out.strip().decode(), graph, idx, color_key, core)
 
     def __nauty_input(self, graph: nx.Graph, color_key:str) -> Tuple[str, Dict[int, Any]]:
         idx = {v:i for i,v in enumerate(graph.nodes())}
-        input_str = ' n={num_atoms} g {edges}. f=[{node_partition}] cxb->>\n'.format(
+        input_str = ' n={num_atoms} g {edges}. f=[{node_partition}] cxb"END\n"->>\n'.format(
             num_atoms=graph.number_of_nodes(),
             edges=self.__nauty_edges(graph, idx),
             node_partition=self.__nauty_node_partition(graph, idx, color_key),
@@ -92,7 +95,7 @@ class Nauty:
         nodes = {v: k for k, v in idx.items()}
         lines = [line.strip() for line in nautstr.split('seconds')[-1].strip().split('\n')]
 
-        adj = [[int(val) for val in line[:-1].split(':')[-1].split()] for line in lines[1:]]
+        adj = [[int(val) for val in line[:-1].split(':')[-1].split()] for line in lines if ':' in line]
 
         if core:
             colors, core = list(zip(*map(

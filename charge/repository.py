eba336ca3945969c55f7b789435ebcd99583cc5c
enitram <martin.engler@gmail.com>
2018-11-28 10:22:40 +0100

    versioning repository

--- charge/repository.py
+++ charge/repository.py
@@ -1,11 +1,14 @@
 import os
-from collections import defaultdict
+from collections import defaultdict, MutableSequence
+from io import IOBase
 from itertools import groupby
-from typing import Dict, List, Tuple, Union
+from typing import Dict, List, Tuple, Union, Optional, AnyStr
+from uuid import uuid4
 from zipfile import ZipFile
 
 import msgpack
 import networkx as nx
+from typing.io import IO
 
 from charge.babel import convert_from, IOType
 from charge.charge_types import Atom
@@ -22,11 +25,14 @@ ChargeSet = Dict[int, Dict[str, List[float]]]
 
 TraceableChargeSet = Dict[int, Dict[str, List[Tuple[float, int, Atom]]]]
 """A collection of possible charges with the molid of the molecule \
-        they came from and the core atom of the neighborhood,
+        they came from and the core atom of the neighborhood, \
+        indexed by shell size and neighborhood canonical key.
 """
 
 
 EitherChargeSet = Union[ChargeSet, TraceableChargeSet]
+
+FileOrFileLike = Union[str, os.PathLike, IOBase, IO[AnyStr]]
 
 
 class Repository:
@@ -35,6 +41,7 @@ class Repository:
     Args:
         min_shell: Minimum shell size in this repository.
         max_shell: Maximum shell size in this repository.
+        nauty: Nauty instance.
 
     Attributes:
         charges_iacm: A dictionary, keyed by shell size, of \
@@ -56,14 +63,21 @@ class Repository:
     """
     def __init__(self,
                  min_shell: int=1,
-                 max_shell: int=7) -> None:
+                 max_shell: int=7,
+                 nauty: Optional[Nauty]=None,
+                 versioning: Optional[bool]=False) -> None:
 
-        self.__nauty = Nauty()
+        self.__nauty = nauty if nauty else Nauty()
         self.__min_shell = max(min_shell, 0)
         self.__max_shell = max_shell
 
-        self.charges_iacm = defaultdict(lambda: defaultdict(list))  # type: EitherChargeSet
-        self.charges_elem = defaultdict(lambda: defaultdict(list))  # type: EitherChargeSet
+        if not versioning:
+            self.charges_iacm = defaultdict(lambda: defaultdict(list))  # type: EitherChargeSet
+            self.charges_elem = defaultdict(lambda: defaultdict(list))  # type: EitherChargeSet
+        else:
+            self.charges_iacm = defaultdict(lambda: defaultdict(_VersioningList))  # type: EitherChargeSet
+            self.charges_elem = defaultdict(lambda: defaultdict(_VersioningList))  # type: EitherChargeSet
+
         self.iso_iacm = defaultdict(list)
         self.iso_elem = defaultdict(list)
 
@@ -73,7 +87,9 @@ class Repository:
             data_type: IOType=IOType.LGF,
             min_shell: int=1,
             max_shell: int=7,
-            traceable: bool=False
+            traceable: Optional[bool]=False,
+            versioning: Optional[bool]=False,
+            nauty: Optional[Nauty]=None
             ) -> 'Repository':
 
         """Creates a new Repository from a directory of files.
@@ -83,11 +99,12 @@ class Repository:
             data_type: Type of the files to read.
             min_shell: Minimum shell size to compute.
             max_shell: Maximum shell size to compute.
+            nauty: Nauty instance.
 
         Returns:
             A new Repository with data read and processed.
         """
-        repo = Repository(min_shell, max_shell)
+        repo = Repository(min_shell, max_shell, nauty, versioning)
         extension = data_type.get_extension()
 
         molids = [int(fn.replace(extension, ''))
@@ -99,30 +116,35 @@ class Repository:
                 molids, data_location, extension, data_type)
 
         # process with iacm atom types
-        repo.charges_iacm = repo.__generate_charges(graphs, 'iacm', traceable)
+        repo.charges_iacm = repo.__generate_charges(graphs, 'iacm', traceable, versioning)
         canons = repo.__make_canons(graphs)
         repo.iso_iacm = repo.__make_isomorphics(molids, canons)
 
         # process as plain elements
-        repo.charges_elem = repo.__generate_charges(graphs, 'atom_type', traceable)
+        repo.charges_elem = repo.__generate_charges(graphs, 'atom_type', traceable, versioning)
         canons = repo.__make_canons(graphs)
         repo.iso_elem = repo.__make_isomorphics(molids, canons)
 
         return repo
 
     @staticmethod
-    def read(location: str = REPO_LOCATION) -> 'Repository':
+    def read(
+            location: Optional[FileOrFileLike] = REPO_LOCATION,
+            versioning: Optional[bool] = False,
+            nauty: Optional[Nauty]=None
+            ) -> 'Repository':
         """Create a Repository by loading from a zip file.
 
         The zip file must have been created by a call to write().
 
         Args:
-            location: Path to the zip file to be read.
+            location: Path to the zip file (or file like object) to be read.
+            nauty: Nauty instance.
 
         Returns:
             A new Repository.
         """
-        repo = Repository()
+        repo = Repository(nauty=nauty, versioning=versioning)
         with ZipFile(location, mode='r') as zf:
             repo.__min_shell, repo.__max_shell = msgpack.unpackb(
                     zf.read('meta'), encoding='utf-8')
@@ -134,13 +156,21 @@ class Repository:
                     zf.read('iso_iacm'), encoding='utf-8')
             repo.iso_elem = msgpack.unpackb(
                     zf.read('iso_elem'), encoding='utf-8')
+
+            if versioning:
+                for _, chdct in repo.charges_iacm.items():
+                    for key, charges in chdct.items():
+                        chdct[key] = _VersioningList(charges)
+                for _, chdct in repo.charges_elem.items():
+                    for key, charges in chdct.items():
+                        chdct[key] = _VersioningList(charges)
         return repo
 
-    def write(self, out: str) -> None:
+    def write(self, out: Optional[FileOrFileLike] = REPO_LOCATION) -> None:
         """Write the repository to disk as a zip file.
 
         Args:
-            out: Path to the zip file to be written.
+            out: Path to the zip file (or file like object) to be written.
         """
         with ZipFile(out, mode='w') as zf:
             zf.writestr('meta', msgpack.packb(
@@ -171,10 +201,14 @@ class Repository:
             self,
             graphs: List[Tuple[int, nx.Graph]],
             color_key: str,
-            traceable: bool=False
+            traceable: bool=False,
+            versioing: bool=False
             ) -> Dict[int, Dict[str, List[float]]]:
         """Generate charges for all shell sizes and neighborhoods."""
-        charges = defaultdict(lambda: defaultdict(list))
+        if not versioing:
+            charges = defaultdict(lambda: defaultdict(list))
+        else:
+            charges = defaultdict(lambda: defaultdict(_VersioningList))
 
         if traceable:
             Worker = _TraceableChargeWorker
@@ -189,7 +223,7 @@ class Repository:
 
         for shell in range(self.__min_shell, self.__max_shell + 1):
             for key, values in charges[shell].items():
-                charges[shell][key] = sorted(values)
+                charges[shell][key].sort()
 
         return charges
 
@@ -283,3 +317,26 @@ class _TraceableChargeWorker:
             charges[key].append((partial_charge, molid, atom))
 
         return charges
+
+
+def proxy():
+
+    changer_methods = {'append', 'clear', 'extend', 'insert', 'pop', 'remove',
+                       '__add__', '__delitem__', '__iadd__', '__init__', '__imul__', '__mul__', '__rmul__', '__setitem__'}
+
+    def proxy_decorator(func):
+        def wrapper(*args, **kw):
+            args[0].version = uuid4().int
+            return func(*args, **kw)
+
+        wrapper.__name__ = func.__name__
+        return wrapper
+
+    dct = list.__dict__.copy()
+    for key, value in dct.items():
+        if key in changer_methods:
+            dct[key] = proxy_decorator(value)
+    return type("proxy_list", (list,), dct)
+
+
+_VersioningList = proxy()

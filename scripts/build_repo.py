eeff81dd421d0780283f506c4f2e9f65b393ea20
Lourens Veen <l.veen@esciencecenter.nl>
2018-12-06 16:58:33 +0100

    Rename cross-validation repo script and add new general one

--- scripts/build_repo.py
+++ scripts/build_repo.py
@@ -1,15 +1,31 @@
-import os
+#!/usr/bin/env python3
+
+import argparse
+from typing import Tuple
 
 from charge.repository import Repository
 
+
+def get_args() -> Tuple[str, str]:
+    parser = argparse.ArgumentParser(
+            description='Create a repository from a directory of LGF files.\n'
+                        'This program reads molecule definitions from LGF'
+                        ' files in a given directory, and processes them into'
+                        ' a repository file for use with charge_assign.')
+    parser.add_argument('-i', '--input', help='Path of the directory with input'
+                        ' files')
+    parser.add_argument('-r', '--repository', help='Path to the repository file'
+                        ' to create')
+    args = parser.parse_args()
+    return args.input, args.repository
+
+
+def main() -> None:
+    source_dir, repo_file = get_args()
+
+    repo = Repository.create_from(source_dir)
+    repo.write(repo_file)
+
+
 if __name__ == '__main__':
-
-    test_data_dir = os.path.realpath(
-            os.path.join(__file__, '..', 'cross_validation_data'))
-
-    out_file = 'cross_validation_repository.zip'
-
-    repo = Repository.create_from(test_data_dir,
-            min_shell=0, max_shell=3, traceable=True)
-
-    repo.write(out_file)
+    main()

f55d2b802bddc555b5d553aafccf1e5b68c95f23
Geller, Richard Michael (rigel100) <richard.geller@uni-duesseldorf.de>
2019-04-23 09:20:01 +0200

    Created Symmetric ILP Charger Test by cloning the ILP Charger Test

--- charge/test/test_chargers.py
+++ charge/test/test_chargers.py
@@ -2,7 +2,7 @@ from math import log
 
 import pytest
 
-from charge.chargers import CDPCharger, DPCharger, ILPCharger, MeanCharger, MedianCharger, ModeCharger
+from charge.chargers import CDPCharger, DPCharger, ILPCharger, MeanCharger, MedianCharger, ModeCharger, SymmetricILPCharger
 
 
 def test_mean_charger(mock_repository, ref_graph):
@@ -91,6 +91,27 @@ def test_mode_charger(mock_repository, ref_graph):
 
 def test_ilp_charger(mock_methane_repository, ref_graph):
     charger = ILPCharger(mock_methane_repository, 2, 10)
+    charger.charge(ref_graph, 0)
+
+    assert ref_graph.node[1]['partial_charge'] == pytest.approx(-0.52)
+    assert ref_graph.node[1]['score'] == pytest.approx(log(4))
+    assert ref_graph.node[2]['partial_charge'] == pytest.approx(0.13)
+    assert ref_graph.node[2]['score'] == pytest.approx(log(3))
+    assert ref_graph.node[3]['partial_charge'] == pytest.approx(0.13)
+    assert ref_graph.node[3]['score'] == pytest.approx(log(3))
+    assert ref_graph.node[4]['partial_charge'] == pytest.approx(0.13)
+    assert ref_graph.node[4]['score'] == pytest.approx(log(3))
+    assert ref_graph.node[5]['partial_charge'] == pytest.approx(0.13)
+    assert ref_graph.node[5]['score'] == pytest.approx(log(3))
+
+    assert ref_graph.graph['time'] < 0.1
+    assert ref_graph.graph['total_charge'] == pytest.approx(0.0)
+    assert ref_graph.graph['total_charge_redist'] == pytest.approx(0.0)
+    assert ref_graph.graph['score'] == pytest.approx(4 * log(3) + log(4))
+
+
+def test_symmetric_ilp_charger(mock_methane_repository, ref_graph):
+    charger = SymmetricILPCharger(mock_methane_repository, 2, 10)
     charger.charge(ref_graph, 0)
 
     assert ref_graph.node[1]['partial_charge'] == pytest.approx(-0.52)

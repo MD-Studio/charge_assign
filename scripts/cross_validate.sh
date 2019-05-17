1bacf9278bf6744e6e08e8d38f07b7424584a464
Geller, Richard Michael (rigel100) <richard.geller@uni-duesseldorf.de>
2019-05-17 09:47:17 +0200

    created script for parallel cross validation

---
+++ scripts/cross_validate.sh
@@ -0,0 +1,22 @@
+source activate charge_assign
+python3 cross_validate.py 0 20 &
+python3 cross_validate.py 1 20 &
+python3 cross_validate.py 2 20 &
+python3 cross_validate.py 3 20 &
+python3 cross_validate.py 4 20 &
+python3 cross_validate.py 5 20 &
+python3 cross_validate.py 6 20 &
+python3 cross_validate.py 7 20 &
+python3 cross_validate.py 8 20 &
+python3 cross_validate.py 9 20 & 
+python3 cross_validate.py 10 20 &
+python3 cross_validate.py 11 20 &
+python3 cross_validate.py 12 20 &
+python3 cross_validate.py 13 20 &
+python3 cross_validate.py 14 20 &
+python3 cross_validate.py 15 20 &
+python3 cross_validate.py 16 20 &
+python3 cross_validate.py 17 20 &
+python3 cross_validate.py 18 20 &
+python3 cross_validate.py 19 20
+
